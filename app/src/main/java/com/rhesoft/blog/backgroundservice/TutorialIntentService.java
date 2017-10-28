15a245a28941985bc8db63c68e00a02e224cc2ec
DESKTOP-M25MNPV\crazyDale <malkin.dmytro@gmail.com>
2017-10-28 20:33:45 +0300

    notification sounds added

--- app/src/main/java/com/rhesoft/blog/backgroundservice/TutorialIntentService.java
+++ app/src/main/java/com/rhesoft/blog/backgroundservice/TutorialIntentService.java
@@ -4,6 +4,9 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 
@@ -32,14 +35,17 @@ public class TutorialIntentService extends IntentService {
 
     protected void showToast(final String msg) {
 
-
         Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
             @Override
             public void run() {
+                Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
                 Notification notification = new Notification.Builder(TutorialIntentService.this)
                         .setContentTitle(msg)
                         .setSmallIcon(R.mipmap.ic_launcher)
+                        .setSound(alarmSound)
+                        .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                        .setLights(Color.RED, 3000, 3000)
                         .build();
                 ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(0, notification);
             }
